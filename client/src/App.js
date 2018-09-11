0653064ba7d8623a4b7e6819c8846636319e32ad
Zach <Zachary.Lynch1337@gmail.com>
2018-09-10 19:57:48 -0700

    Created appNavBar component as a class based component to display nav bar on screen with link to github

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import './components/AppNavBar';
+import AppNavbar from './components/AppNavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Hello!</h1>
+          <AppNavbar></AppNavbar>
         </header>
       </div>
     );
